81ae84a30a1e838bb8847686fcc7a66a92ae9058
JooHyeonKim <subin_282@naver.com>
2023-02-06 12:21:54 +0900

    Initial Commit

---
+++ MyFirstiOs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstiOs
+//
+//  Created by 김주현 on 2023/02/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
